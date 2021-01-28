44f312a83900fd0fcb219c6cef7ec64dddf50ce4
avkgeo <avakya.georgiy00@gmail.com>
2021-01-28 12:10:47 +0300

    first

---
+++ app/src/main/java/com/ithub/lesson2/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.ithub.lesson2
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+    private lateinit var edName: EditText
+    private lateinit var edAge: EditText
+    private lateinit var resultTV: TextView
+
+    private val code = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+//    val intent = Intent(Intent.ACTION_SEND) // не явные Intent
+        //            intent.type = "text/html"
+//            intent.putExtra(Intent.EXTRA_EMAIL, "emailaddress@emailaddress.com")
+//            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject")
+//            intent.putExtra(Intent.EXTRA_TEXT, "I'm email body.")
+//            startActivity(Intent.createChooser(intent, "Send Email"))
+
+
+        edAge = findViewById(R.id.edit_text_age)
+        edName = findViewById(R.id.edit_text_name)
+        resultTV = findViewById(R.id.result)
+
+
+        findViewById<Button>(R.id.next).setOnClickListener {
+            next(edName.text.toString(), edAge.text.toString())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null && requestCode == code) {
+            resultTV.text = data.getStringExtra("result")
+        }
+    }
+
+    fun next(name: String, age: String) {
+        if (name.isNotEmpty() && age.isNotEmpty()) {
+            val intent = Intent(
+                this,
+                MainActivity2::class.java
+            ) // переход на активити MainActivity2 явный intent
+            intent.putExtra("age", age.toInt())
+            intent.putExtra("name", name)
+//            startActivity(intent)
+            startActivityForResult(intent, code) // положительное число
+        } else {
+            Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
---
+++ app/src/main/java/com/ithub/lesson2/MainActivity2.kt
@@ -0,0 +1,40 @@
+package com.ithub.lesson2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var edFinish: EditText
+    private lateinit var finish: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main2_activity)
+
+        val thisIntent = intent // intent == getIntent/setIntent
+        textView = findViewById(R.id.text_view)
+        edFinish = findViewById(R.id.res_ed)
+        finish = findViewById(R.id.finish)
+        val name = thisIntent.getStringExtra("name")
+        val age = thisIntent.getIntExtra("age", 0)
+
+
+
+        textView.text =
+            "Имя = ${thisIntent.getStringExtra("name")} \nВозраст = $age" // text = "smthg" equals setText("smthg")
+
+        finish.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra("result", if (edFinish.text.toString().isNotEmpty()) edFinish.text.toString() else "Не данных")
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
+}
